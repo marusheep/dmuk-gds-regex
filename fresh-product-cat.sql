ac56d17b295128e220cc06a6b9b7794fd381ab3c
marusheep <38782965+marusheep@users.noreply.github.com>
2020-07-15 22:43:56 +0100

    Update fresh-product-cat.sql
    add
    .*moisturizer$

--- fresh-product-cat.sql
+++ fresh-product-cat.sql
@@ -3,6 +3,6 @@ CASE
     WHEN REGEXP_MATCH(Product Name, '(?i).*parfum.*|(?i).*candle.*') THEN "Fragrance" 
     WHEN REGEXP_MATCH(Product Name, '(?i).*shower.*gel$|(?i).*body.*lotion$|(?i).*antiperspirant$|(?i).*body.*polish.*|(?i).*soap$|(?i).*hand.*cream$|(?i).*bath.*|(?i).*body.*cream$|(?i).*shampoo.*|(?i).*conditioner.*') THEN "Bodycare" 
     WHEN REGEXP_MATCH(Product Name, '(?i).*lip .*') THEN "Lip Care"
-    WHEN REGEXP_MATCH(Product Name, '(?i).*serum$|(?i).*mask.*|(?i).*essence.*|(?i).*cleanser$|(?i).*toner.*|(?i).*face wash$|(?i).*moisturiser$|(?i).*night cream$|(?i).*face.*|(?i).*eye.*|(?i).*crème.*|(?i).*cleansing oil$|(?i).*lotion$|(?i).*skin nutrition booster$|(?i).*purifying treatment bar.*|(?i).*brightening powder$') THEN "Skincare" 
+    WHEN REGEXP_MATCH(Product Name, '(?i).*serum$|(?i).*mask.*|(?i).*essence.*|(?i).*cleanser$|(?i).*toner.*|(?i).*moisturiser.*|.*moisturizer.*|(?i).*night cream.*|(?i).*face.*|(?i).*eye.*|(?i).*crème.*|(?i).*cleansing oil$|(?i).*lotion$|(?i).*skin nutrition booster$|(?i).*purifying treatment bar.*|(?i).*brightening powder$') THEN "Skincare" 
     ELSE "Other"
 END
