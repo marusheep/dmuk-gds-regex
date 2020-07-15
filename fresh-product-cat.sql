0107ead2e1ee55ec731d4651dcf02d91aee91b9a
marusheep <38782965+marusheep@users.noreply.github.com>
2020-07-15 22:58:20 +0100

    Update fresh-product-cat.sql
    add .*gift.* for gift cat

--- fresh-product-cat.sql
+++ fresh-product-cat.sql
@@ -1,5 +1,5 @@
 CASE
-    WHEN REGEXP_MATCH(Product Name, '(?i).*set$') THEN "Gift Set" 
+    WHEN REGEXP_MATCH(Product Name, '(?i).*set$|(?i).*gift.*') THEN "Gift Set" 
     WHEN REGEXP_MATCH(Product Name, '(?i).*parfum.*|(?i).*candle.*') THEN "Fragrance" 
     WHEN REGEXP_MATCH(Product Name, '(?i).*shower.*gel$|(?i).*body.*lotion$|(?i).*antiperspirant$|(?i).*body.*polish.*|(?i).*soap$|(?i).*hand.*cream$|(?i).*bath.*|(?i).*body.*cream$|(?i).*shampoo.*|(?i).*conditioner.*') THEN "Bodycare" 
     WHEN REGEXP_MATCH(Product Name, '(?i).*lip .*') THEN "Lip Care"
