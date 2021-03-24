15effeac3a329f68b019f4e0907e28c324566c72
marusheep <38782965+marusheep@users.noreply.github.com>
2021-03-24 17:19:48 +0000

    new

---
+++ unineed-uk-product-function.sql
@@ -0,0 +1,30 @@
+CASE
+WHEN Product Category = "Skin Care" THEN
+
+CASE
+	WHEN REGEXP_MATCH(Product Name,'.*(?i)Scrub.*|.*Erno Laszlo.*Sea Mud.*.*')
+    	THEN "Exfoliating"
+    
+    WHEN REGEXP_MATCH(Product Name,'.*(?i)Hydra.*|.*(?i)Mist.*|.*Lancôme.*Génifique.*|.*Lancôme.*Génifique.*|.*Lancome.*Génifique.*|.*Lancome.*confort tonique.*|.*Elizabeth Arden.*Visible Difference.*Refining Moisture.*|.*Erno Laszlo.*Phelityl.*|.*Filorga.*Foam.*|.*Clinique.*Moisture Surge.*')
+    	THEN "Hydrating"
+    
+    WHEN REGEXP_MATCH(Product Name,'.*Filorga.*Meso.*Mask.*|.*Clarins.*(?i)super restorative.*|.*Filorga.*Time-Zero.*|.*Valmont.*V-Shap.*|.*Shiseido.*Bio-Performance.*')
+    	THEN "Anti Wrinkle"
+
+    WHEN REGEXP_MATCH(Product Name,'.*(?i)Repair.*|.*SK-II.*Facial Treatment.*|.*Pitera.*')
+    	THEN "Repair"
+    
+    WHEN REGEXP_MATCH(Product Name,'.*Brightening.*|.*SK-II.*Aura.*')
+    	THEN "Whitening"
+
+    WHEN REGEXP_MATCH(Product Name,'.*EltaMD.*Foaming.*|.*Dr.*Andrew.*Origins.*|.*Kiehl.*Calendula.*')
+    	THEN "Anti Inflammatory"
+
+    WHEN REGEXP_MATCH(Product Name,'.*(?i)Oxygen.*|.*Valmont.*Prime.*')
+    	THEN "Anti Oxydation"
+        
+    WHEN REGEXP_MATCH(Product Name,'.*(?i)Anti-Ageing.*|.*(?i)Anti.*Aging.*|.*(?i)Aging Protect.*|.*(?i)Youth.*Restoring.*|.*Elizabeth Arden.*Ceramide.*|.*Filorga.*Optim-Eyes.*|.*Filorga.*Time.*Filler.*|.*Filorga.*NCEF.*|.*Sisley.*Ecological.*|.*SK-II.*Power.*|.*La Mer.*The Treatment.*|.*Elizabeth Arden.*Ceramide Radiance.*|.*Shiseido.*Ultimune Power.*|.*Clarins.*Double Serum.*|.*La Prairie.*Caviar.*|.*La Mer.*Crème.*')
+    	THEN "Anti Aging"
+END
+
+END
