ef232ec5b8a64a09a11640c30eb79eaca2b47de7
karen bull <marusheep@gmail.com>
2021-04-23 14:29:51 +0100

    update

--- Hairburst-uk-product-category.sql
+++ Hairburst-uk-product-category.sql
@@ -2,5 +2,7 @@ CASE
   WHEN REGEXP_MATCH(Product Name, '.*(?i)bundle.*|.*(?i)set.*') THEN "Bundle"
   WHEN REGEXP_MATCH(Product Name, '.*(?i)vitamin.*|.*(?i)capsule.*') THEN "Vitamins"
   WHEN REGEXP_MATCH(Product Name, '.*(?i)shampoo.*|.*(?i)conditioner.*') THEN "Shampoo & Conditioner"
+  WHEN REGEXP_MATCH(Product Name,'.*(?i)Elixir.*|.*(?i)serum.*') THEN "Elixir"
+  WHEN REGEXP_MATCH(Product Name,'.*(?i)brush.*|.*(?i)scalp roller.*') THEN "Tools"
   ELSE "Others"
 END
