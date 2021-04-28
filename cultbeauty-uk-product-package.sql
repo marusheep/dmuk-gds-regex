6b3f4af7ba640fb30ff9965540fe71c6af71af69
karen bull <marusheep@gmail.com>
2021-04-28 16:52:22 +0100

    new file

---
+++ cultbeauty-uk-product-package.sql
@@ -0,0 +1,5 @@
+CASE
+    WHEN REGEXP_MATCH(Product Name,'.*(?i)tester.*|.*(?i)sample.*') THEN "Trial Set"
+    WHEN REGEXP_MATCH(Product Name,'.*(?i)gift.*|.*GWP.*') THEN "Gift set"
+    WHEN REGEXP_MATCH(Product Name,'.*(?i)bundle.*|.*(?i)pack.*|.*(?i)Duo.*|.*(?i)Collection.*|.*(?i) Box.*|.* Set$') THEN "Bundle set"
+END
