c88f0830f7e6502cb807d98decb5935bd7e1c34c
karen bull <marusheep@gmail.com>
2021-04-30 11:12:05 +0100

    new

---
+++ houseoffraser-product-package.sql
@@ -0,0 +1,5 @@
+CASE
+    WHEN REGEXP_MATCH(Product Name,'.*(?i)tester.*|.*(?i)sample.*') THEN "Trial Set"
+    WHEN REGEXP_MATCH(Product Name,'.*(?i)gift.*|.*GWP.*') THEN "Gift set"
+    WHEN REGEXP_MATCH(Product Name,'.*(?i)bundle.*|.*(?i) set.*|.*(?i)pack.*|.*(?i)kit.*|.*(?i)Duo.*|.*(?i)Collection.*|.*(?i) Box.*') THEN "Bundle set"
+END
